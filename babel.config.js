b04462829082cb4660541e8b1298cb42dfe84e64
YuTengjing <ytj2713151713@gmail.com>
2020-01-22 23:53:39 +0800

    :wrench: configs: add some useful react babel plugin when dev

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,7 @@ const envPreset = [
     {
         useBuiltIns: 'usage',
         targets: {
-            browsers: {
-                electron: '7.1.9',
-            },
+            electron: '7.1.9',
         },
         corejs: 3,
     },
@@ -24,13 +22,15 @@ module.exports = api => {
     api.cache(true);
 
     return {
-        presets: ['@babel/preset-typescript', '@babel/preset-react', envPreset],
+        presets: ['@babel/preset-typescript', envPreset],
         plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-optional-chaining', importPlugin],
         env: {
             development: {
+                presets: [['@babel/preset-react', { development: true }]],
                 plugins: ['react-hot-loader/babel'],
             },
             production: {
+                presets: ['@babel/preset-react'],
                 plugins: [
                     '@babel/plugin-transform-react-constant-elements',
                     '@babel/plugin-transform-react-inline-elements',
